42cb3f7d6f0659db08fa8d0a8f80ce0d4e9260d6
Warus <kwarnello@gmail.com>
2017-07-28 22:19:00 +0200

    no message

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,9 +1,12 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DataLoader {
 
+	Scanner scanner = new Scanner(System.in);
 	Random random = new Random();
 	Particle particle;
+	int n = 0;
 
 	// in cm
 	final double detectorSize = 42.4, detectorFreeSpaceHigh = 20;
@@ -19,22 +22,35 @@ public class DataLoader {
 
 	DataLoader() {
 		numberOfDetected = 0;
-		xSampleSize = 10;
-		ySampleSize = 10;
-		//zSampleSize = 10;
-		numberOfTry = 1000000;
 
-		minRange = 3.3;
-		maxRange = 13.3;
+		System.out.println("The sample X size (in cm):");
+		xSampleSize = scanner.nextDouble();
+
+		System.out.println("The sample Y size (in cm):");
+		ySampleSize = scanner.nextDouble();
+
+		System.out.println("Minimum distance travelled in the sample (in microns):");
+		minRange = scanner.nextDouble();
+
+		System.out.println("Maximum distance travelled in the sample (in microns):");
+		maxRange = scanner.nextDouble();
+
+		System.out.println("Number of particles generated (in thousands):");
+		numberOfTry = 1000 * scanner.nextInt();
 
 		for (int i = 1; i <= numberOfTry; i++) {
-			particle = new Particle(xSampleSize, ySampleSize, zSampleSize, minRange, maxRange);
-			number++;
+			n++;
+			if (n % 10000 == 0) {
+				double x = (double) numberOfDetected / n * 100000.0;
+				System.out.println(n + ": " + 100 * Math.round(x) / 100000.0 + "%");
+			}
+
+			particle = new Particle(xSampleSize, ySampleSize, zSampleSize, minRange, maxRange, detectorSize,
+					detectorFreeSpaceHigh);
 		}
 
-		double x = (double) numberOfDetected / numberOfTry * 1.0;
-		System.out.println(x);
-		System.out.println(number);
+		double x = (double) numberOfDetected / numberOfTry * 100000.0;
+		System.out.println("Percentage of detected particles: " + 100 * Math.round(x) / 100000.0 + "%");
 	}
 
 	static void addNumberOfDetected() {
--- src/Particle.java
+++ src/Particle.java
@@ -5,31 +5,46 @@ public class Particle {
 	Random random = new Random();
 
 	double[] particleStartingCoor = new double[3];
+	double[] particleStartingArgonCoor = new double[3];
 	double[] particleCoor = new double[3];
 	double xSampleSize, ySampleSize, zSampleSize, min, max;
 	double[] vector = new double[3];
 	double distanceTravelled;
 	boolean detected;
+	double detectorSize, detectorFreeSpaceHigh;
 
-	Particle(double xSampleSize, double ySampleSize, double zSampleSize, double min, double max) {
+	// in MeV
+	double[] energy = { 4.5, 4.2, 4.0, 3.7, 3.3, 2.9, 2.4, 2.0, 1.6, 1.0 };
+	// in microns
+	double[] minimumDistanceTravelledInArgonToDetected = { 10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000,
+			1000 };
+	double distanceInArgon = 0.0, distanceTravelledInArgon;
 
+	boolean exitCube = true;
+
+	// energy of particle in MeV
+	double energyOfParticle = 0.0;
+
+	Particle(double xSampleSize, double ySampleSize, double zSampleSize, double min, double max, double detectorSize,
+			double detectorFreeSpaceHigh) {
 		this.xSampleSize = xSampleSize;
 		this.ySampleSize = xSampleSize;
 		this.zSampleSize = xSampleSize;
 		this.min = min;
 		this.max = max;
+		this.detectorSize = detectorSize;
+		this.detectorFreeSpaceHigh = detectorFreeSpaceHigh;
 
 		createDecay();
-		// printCoordinates();
 		flyInRandomDirection();
 		calcTravel();
 		checkIfDetected();
-		printCoordinates();
+		// printCoordinates();
 	}
 
 	private void createDecay() {
-		particleCoor[0] = random.nextDouble() * xSampleSize;
-		particleCoor[1] = random.nextDouble() * ySampleSize;
+		particleCoor[0] = ((detectorSize - xSampleSize) / 2) + random.nextDouble() * xSampleSize;
+		particleCoor[1] = ((detectorSize - ySampleSize) / 2) + random.nextDouble() * ySampleSize;
 		particleCoor[2] = -random.nextDouble() * max / 10000;
 
 		particleStartingCoor[0] = particleCoor[0];
@@ -38,13 +53,12 @@ public class Particle {
 	}
 
 	private void printCoordinates() {
-		// System.out.println(
-		// "{" + particleStartingCoor[0] + ", " + particleStartingCoor[1] + ", "
-		// + particleStartingCoor[2] + "}");
-		// System.out.println("{" + particleCoor[0] + ", " + particleCoor[1] +
-		// ", " + particleCoor[2] + "}");
-//		System.out.println(vector[0] + " " + vector[1] + " " + vector[2]);
-		//System.out.println(distanceTravelled);
+		System.out.println("Startowe koordynaty: {" + particleStartingCoor[0] + ", " + particleStartingCoor[1] + ", "
+				+ particleStartingCoor[2] + "}. Koñcowe koordynaty: {" + particleCoor[0] + ", " + particleCoor[1] + ", "
+				+ particleCoor[2] + "}. \n Energia po przejœciu przez próbkê: " + energyOfParticle
+				+ "MeV, po przejœciu w próbce " + distanceTravelled + ". W Argonie przesz³a: "
+				+ distanceTravelledInArgon);
+
 	}
 
 	private void flyInRandomDirection() {
@@ -82,8 +96,73 @@ public class Particle {
 	}
 
 	private void checkIfDetected() {
-		if (distanceTravelled < max && distanceTravelled >= min)
-			DataLoader.addNumberOfDetected();
+		// if (distanceTravelled < max && distanceTravelled >= min)
+		// DataLoader.addNumberOfDetected();
+		if (distanceTravelled < max && distanceTravelled >= min) {
+			checEnergyOfParticleAfterSampleExit();
+			checkTravelThroughArgon();
+			if (exitCube == false)
+				DataLoader.addNumberOfDetected();
+		}
+	}
+
+	private void checkTravelThroughArgon() {
+		particleStartingArgonCoor[0] = particleCoor[0];
+		particleStartingArgonCoor[1] = particleCoor[1];
+		particleStartingArgonCoor[2] = particleCoor[2];
+
+		int n = 0;
+		do {
+			n++;
+			particleCoor[0] += (vector[0] / 10000);
+			particleCoor[1] += (vector[1] / 10000);
+			particleCoor[2] += (vector[2] / 10000);
+		} while (n < distanceInArgon * 1.01 && particleCoor[1] > 0 && particleCoor[1] < detectorSize
+				&& particleCoor[0] > 0 && particleCoor[0] < detectorSize);
+
+		distanceTravelledInArgon = (Math.sqrt(Math.pow(particleCoor[0] - particleStartingArgonCoor[0], 2)
+				+ Math.pow(particleCoor[1] - particleStartingArgonCoor[1], 2)
+				+ Math.pow(particleCoor[2] - particleStartingArgonCoor[2], 2))) * 10000;
+
+
+		if (distanceTravelledInArgon >= distanceInArgon)
+			exitCube = false;
+
+	}
+
+	private void checEnergyOfParticleAfterSampleExit() {
+		if (distanceTravelled >= min && distanceTravelled < (min + (max - min) * 0.1)) {
+			energyOfParticle = energy[0];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[0];
+		} else if (distanceTravelled >= (min + (max - min) * 0.1) && distanceTravelled < (min + (max - min) * 0.2)) {
+			energyOfParticle = energy[1];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[1];
+		} else if (distanceTravelled >= (min + (max - min) * 0.2) && distanceTravelled < (min + (max - min) * 0.3)) {
+			energyOfParticle = energy[2];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[2];
+		} else if (distanceTravelled >= (min + (max - min) * 0.3) && distanceTravelled < (min + (max - min) * 0.4)) {
+			energyOfParticle = energy[3];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[3];
+		} else if (distanceTravelled >= (min + (max - min) * 0.4) && distanceTravelled < (min + (max - min) * 0.5)) {
+			energyOfParticle = energy[4];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[4];
+		} else if (distanceTravelled >= (min + (max - min) * 0.5) && distanceTravelled < (min + (max - min) * 0.6)) {
+			energyOfParticle = energy[5];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[5];
+		} else if (distanceTravelled >= (min + (max - min) * 0.6) && distanceTravelled < (min + (max - min) * 0.7)) {
+			energyOfParticle = energy[6];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[6];
+		} else if (distanceTravelled >= (min + (max - min) * 0.7) && distanceTravelled < (min + (max - min) * 0.8)) {
+			energyOfParticle = energy[7];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[7];
+		} else if (distanceTravelled >= (min + (max - min) * 0.8) && distanceTravelled < (min + (max - min) * 0.9)) {
+			energyOfParticle = energy[8];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[8];
+		} else if (distanceTravelled >= (min + (max - min) * 0.9) && distanceTravelled <= (min + (max - min) * 1)) {
+			energyOfParticle = energy[9];
+			distanceInArgon = minimumDistanceTravelledInArgonToDetected[9];
+		}
+
 	}
 
 }
